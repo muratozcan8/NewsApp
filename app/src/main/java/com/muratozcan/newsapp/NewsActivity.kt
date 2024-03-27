2c7fb0601c6c25ca5996659ee0df8a4edaf7ffb4
Murat ozcan <mrtzcn08@gmail.com>
2024-03-28 01:10:16 +0300

    set up navigation

--- app/src/main/java/com/muratozcan/newsapp/NewsActivity.kt
+++ app/src/main/java/com/muratozcan/newsapp/NewsActivity.kt
@@ -2,6 +2,11 @@ package com.muratozcan.newsapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.muratozcan.newsapp.databinding.ActivityNewsBinding
 
 class NewsActivity : AppCompatActivity() {
@@ -11,6 +16,11 @@ class NewsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityNewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val fragment = navHostFragment.childFragmentManager.fragments[0]
+
+        binding.bottomNavigationView.setupWithNavController(fragment.findNavController())
 
     }
 }
